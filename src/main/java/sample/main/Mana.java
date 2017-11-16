1fc9c0dc0a0f66d6813f790b79b246bd98de9d0d
zany <muguliebe@gmail.com>
2017-11-16 14:38:35 +0900

    ing

--- src/main/java/sample/main/Mana.java
+++ src/main/java/sample/main/Mana.java
@@ -1,18 +1,8 @@
 package sample.main;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-
 public class Mana {
 
-	public static void main(String[] args) throws Exception{
-		List<Integer> integers = Arrays.asList(1,2,3,4,5);
-		
-		integers.stream()
-			.reduce((a,b) -> a+b)
-			.ifPresent(System.out::println);
-	}
-	
+    public static void main(String[] args) throws Exception {
+        System.out.println("go1");
+    }
 }
