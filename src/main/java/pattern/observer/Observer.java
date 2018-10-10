d65365f535c3188ac3ea71c42a8fff435143d120
zany <muguliebe@gmail.com>
2018-10-10 17:58:44 +0900

    added observer pattern

---
+++ src/main/java/pattern/observer/Observer.java
@@ -0,0 +1,5 @@
+package pattern.observer;
+
+public interface Observer<T> {
+   void observe(T event);
+}
---
+++ src/test/java/com/ObserverTest.java
@@ -0,0 +1,41 @@
+package pattern;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import pattern.observer.*;
+
+import static org.mockito.Mockito.times;
+
+public class ObserverTest {
+    @Test
+    public void observersHandleEventsFromSubject() {
+        // given
+        Subject<String> subject = new ConcreteSubject();
+        Observer<String> observerA = Mockito.spy(new ConcreteObserverA());
+        Observer<String> observerB = Mockito.spy(new ConcreteObserverB());
+
+        // when
+        subject.notifyObservers("No listeners");
+
+        subject.registerObserver(observerA);
+        subject.notifyObservers("Message for A");
+
+        subject.registerObserver(observerB);
+        subject.notifyObservers("Message for A & B");
+
+        subject.unregisterObserver(observerA);
+        subject.notifyObservers("Message for B");
+
+        subject.unregisterObserver(observerB);
+        subject.notifyObservers("No listeners");
+
+        // then
+        Mockito.verify(observerA, times(1)).observe("Message for A");
+        Mockito.verify(observerA, times(1)).observe("Message for A & B");
+        Mockito.verifyNoMoreInteractions(observerA);
+
+        Mockito.verify(observerB, times(1)).observe("Message for A & B");
+        Mockito.verify(observerB, times(1)).observe("Message for B");
+        Mockito.verifyNoMoreInteractions(observerB);
+    }
+}
